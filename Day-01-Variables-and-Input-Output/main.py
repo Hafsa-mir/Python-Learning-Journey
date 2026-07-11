5695b2200c88ee10f06ea61bebd30b18f2c82e03
Hafsa Mir <hafsamir980@gmail.com>
2026-07-12 03:05:30 +0500

    Add Day 01- Variables & Input/Output

---
+++ Day-01-Variables-and-Input-Output/main.py
@@ -0,0 +1,16 @@
+# Student Profile Generator
+
+name= input("Enter Your Name:")
+age =int(input("Enter Your Age:"))
+degree= input("Enter Your Degree:")
+skills= input("Enter Your Skills:")
+dream_job = input("Enter Your Dream Job:")
+
+print()
+print("=====Student Profile=====")
+print("Name:",name)
+print("Age:",age)
+print("Degree:",degree)
+print("Skills:",skills)
+print("Dream Job:",dream_job)
+print("===============================")
