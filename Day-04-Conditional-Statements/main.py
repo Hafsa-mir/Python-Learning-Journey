25f2bc95f62ac7bf4f3430678a811d649a79fae6
Hafsa Mir <hafsamir980@gmail.com>
2026-07-14 02:37:50 +0500

    Add Day 04 - Conditional Statements
    Added a simple AI tool recommendation system based on user choice.

--- Day-04-Conditional-Statements/main.py
+++ Day-04-Conditional-Statements/main.py
@@ -1,1 +1,17 @@
+# AI Tool Recommendation System
 
+print("Choose your task:")
+print("1. Coding\n2. Research\n3. Image Generation\n4. Presentation")
+
+choice = int(input("Enter your Choice: "))
+
+if choice == 1:
+    print("Claude")
+elif choice == 2:
+    print("Perplexity")
+elif choice == 3:
+    print("Midjourney")
+elif choice == 4:
+    print("Gamma")
+else:
+    print("Invalid choice")
