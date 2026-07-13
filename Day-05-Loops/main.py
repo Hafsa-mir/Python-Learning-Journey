5263a771f213ae4e9c988572c466ecab220e131a
Hafsa Mir <hafsamir980@gmail.com>
2026-07-14 02:52:17 +0500

    Add Day 05 - Loops

--- Day-05-Loops/main.py
+++ Day-05-Loops/main.py
@@ -1,1 +1,15 @@
 
+# Multiplication Table Generator
+# Using for loop:
+
+n = int(input("Enter a number: "))
+for i in range(1, 11):
+    print(f"{n} X {i} = {n * i}")
+
+# Using while loop:
+
+n = int(input("Enter a number: "))
+i = 1
+while i <= 10:
+    print(f"{n} X {i} = {n * i}")
+    i += 1
