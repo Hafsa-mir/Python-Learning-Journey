d2e49248d9433ce48044c8307901ef8e6ed09180
Hafsa Mir <hafsamir980@gmail.com>
2026-07-14 02:20:21 +0500

    Add Day 03 - Operators
    Added a screen time calculator that takes input for mobile, laptop, and TV usage, and calculates total and average screen time, as well as comparisons and logical evaluations.

--- Day-03-Operators/main.py
+++ Day-03-Operators/main.py
@@ -1,1 +1,23 @@
+# ==========================================
+# Project: Screen Time Calculator
+# Topic: Python Operators Mini Project
+# ==========================================
+
+mobile_hours = float(input("Enter Mobile Screen Time (hours): "))
+laptop_hours = float(input("Enter Laptop Screen Time (hours): "))
+tv_hours = float(input("Enter TV Screen Time (hours): "))
+
+# Arithmetic Operators
+total_screen_time = mobile_hours + laptop_hours + tv_hours
+average_screen_time = total_screen_time / 3
+remaining_time = 8 - total_screen_time
+
+# Comparison Operators
+more_than_8_hours = total_screen_time > 8
+mobile_more_than_laptop = mobile_hours > laptop_hours
+laptop_equal_tv = laptop_hours == tv_hours
+
+# Logical Operators
+healthy_usage = total_screen_time <= 8 and mobile_hours <= 5
+high_usage = total_screen_time > 8 or mobile_hours > 5
 
